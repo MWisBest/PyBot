ce45c2f8a97813de7b88260bbebcf1d0fc27bc77
Glitch <iGlitch@users.noreply.github.com>
2015-08-01 16:29:25 -0400

    Update zipcode.py

--- Commands/zipcode/zipcode.py
+++ Commands/zipcode/zipcode.py
@@ -1,6 +1,7 @@
 ###########################################################################
 ## PyBot                                                                 ##
 ## Copyright (C) 2015, Kyle Repinski                                     ##
+## Copyright (C) 2015, Andres Preciado (Glitch)                          ##
 ##                                                                       ##
 ## This program is free software: you can redistribute it and/or modify  ##
 ## it under the terms of the GNU General Public License as published by  ##
